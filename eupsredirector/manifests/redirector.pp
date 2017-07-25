d4660a03a7750c1bd7baf66d3d9ddcf8bf342689
Joshua Hoblitt <josh@hoblitt.com>
2017-07-25 16:08:41 -0700

    cleanup nginx config/redirect

--- eupsredirector/manifests/redirector.pp
+++ eupsredirector/manifests/redirector.pp
@@ -4,17 +4,13 @@ include ::nginx
 
 class { 'timezone': timezone  => 'US/Pacific' }
 
-$pkgs = ['git', 'tree', 'vim-enhanced', 'ack', 'rsync']
-ensure_packages($pkgs)
-# ensure_packages() doesn't work correctly with resource collector deps
+# $pkgs = ['git', 'tree', 'vim-enhanced', 'ack', 'rsync']
+# ensure_packages($pkgs)
 
-$eupspkg_host       = hiera('eupspkg_host', 'eupspkg-test')
-$eupspkg_access_log = "/var/log/nginx/${eupspkg_host}.access.log"
-$eupspkg_error_log  = "/var/log/nginx/${eupspkg_host}.error.log"
-
-if $ssl_cert and $ssl_key {
-  $enable_ssl = true
-}
+$redirect_host       = hiera('redirect_host', 'eups-redirect.lsst.codes')
+$eupspkg_host        = hiera('eupspkg_host', 'eups.lsst.codes')
+$redirect_access_log = "/var/log/nginx/${redirect_host}.access.log"
+$redirect_error_log  = "/var/log/nginx/${redirect_host}.error.log"
 
 selboolean { 'httpd_can_network_connect':
   value      => on,
@@ -30,25 +26,25 @@ selboolean { 'httpd_setrlimit':
 # the canonical https URL in one step.  If we do a http -> https redirect, as
 # is enabled by puppet-nginx's rewrite_to_https param, the the U-A will catch
 # a certificate error before getting to the redirect to the canonical name.
-$eupspkg_raw_prepend = [
+$redirect_raw_prepend = [
   "if ( \$host != \'${eupspkg_host}\' ) {",
-  "  return 301 https://${eupspkg_host}\$request_uri;",
+  "  rewrite ^/eupspkg/(.*)$ https://${eupspkg_host}/stack/src/$1 last;",
   '}',
 ]
 
-nginx::resource::vhost { $eupspkg_host:
+nginx::resource::vhost { $redirect_host:
   ensure               => present,
   server_name          => [
-    $eupspkg_host,
+    $redirect_host,
     'sw.lsstcorp.org',
   ],
   listen_port          => 80,
   ssl                  => false,
-  access_log           => $eupspkg_access_log,
-  error_log            => $eupspkg_error_log,
+  access_log           => $redirect_access_log,
+  error_log            => $redirect_error_log,
   rewrite_to_https     => false,
   use_default_location => false,
   index_files          => [],
   # see comment above $raw_prepend declaration
-  raw_prepend          => $eupspkg_raw_prepend,
+  raw_prepend          => $redirect_raw_prepend,
 }
