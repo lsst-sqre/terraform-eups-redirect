1a2399738c42abdea26925de48b2d8a1e91b7940
Joshua Hoblitt <josh@hoblitt.com>
2018-06-18 14:02:38 -0700

    fix tls cert paths (again)

--- tf/ssl-proxy-secret.tf
+++ tf/ssl-proxy-secret.tf
@@ -5,8 +5,8 @@ resource "kubernetes_secret" "ssl_proxy" {
   }
 
   data {
-    proxycert = "${file("${path.root}/lsst-certs/lsst.codes/sw.lsstcorp.org/sw.lsstcorp.org.20170530_chain.pem")}"
-    proxykey  = "${file("${path.root}/lsst-certs/lsst.codes/sw.lsstcorp.org/sw.lsstcorp.org.20170530.key")}"
+    proxycert = "${file("${path.root}/lsst-certs/sw.lsstcorp.org/sw.lsstcorp.org.20170530_chain.pem")}"
+    proxykey  = "${file("${path.root}/lsst-certs/sw.lsstcorp.org/sw.lsstcorp.org.20170530.key")}"
     dhparam   = "${file("${path.root}/dhparam.pem")}"
   }
 }
